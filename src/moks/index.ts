3e07d80c30f17c31d391e193be271eb01004e2d9
IvanovMikl <106914813+IvanovMikl@users.noreply.github.com>
2023-02-12 03:20:59 +0300

    init repository

---
+++ src/moks/index.ts
@@ -0,0 +1,317 @@
+export const users = [
+  {
+    "_id": "63e16fb054d3b00b881302ec",
+    "index": 0,
+    "guid": "a16a92a2-fc48-4e25-8d4c-a710dc468f31",
+    "isActive": true,
+    "balance": "$3,377.43",
+    "picture": "http://placehold.it/32x32",
+    "age": 23,
+    "eyeColor": "blue",
+    "name": "Stuart Dyer",
+    "gender": "male",
+    "company": "EXOSIS",
+    "email": "stuartdyer@exosis.com",
+    "phone": "+1 (810) 469-3994",
+    "address": "515 Lott Avenue, Ypsilanti, Nebraska, 434",
+    "about": "Ea esse irure nisi incididunt non voluptate. Aute sint eiusmod est duis proident ullamco. Incididunt culpa ut laborum qui et consectetur duis ullamco laborum. Proident excepteur reprehenderit commodo elit do in irure minim labore elit aliqua culpa. Mollit quis adipisicing enim quis eiusmod exercitation velit quis dolor reprehenderit eu.\r\n",
+    "registered": "2021-05-29T11:25:55 -03:00",
+    "latitude": -31.551836,
+    "longitude": 91.839577,
+    "tags": [
+      "dolore",
+      "ut",
+      "do",
+      "irure",
+      "dolor",
+      "incididunt",
+      "in"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Therese Kim"
+      },
+      {
+        "id": 1,
+        "name": "Price Craft"
+      },
+      {
+        "id": 2,
+        "name": "Lea Nichols"
+      }
+    ],
+    "greeting": "Hello, Stuart Dyer! You have 7 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "63e16fb0a7b820e2574fc5fd",
+    "index": 1,
+    "guid": "4290d64f-a696-47df-b17c-0d0769bef4c8",
+    "isActive": false,
+    "balance": "$1,122.44",
+    "picture": "http://placehold.it/32x32",
+    "age": 36,
+    "eyeColor": "green",
+    "name": "Helga Hooper",
+    "gender": "female",
+    "company": "OATFARM",
+    "email": "helgahooper@oatfarm.com",
+    "phone": "+1 (861) 490-3518",
+    "address": "153 Stillwell Place, Lindcove, North Carolina, 1204",
+    "about": "Incididunt aliqua sunt fugiat do aliquip qui adipisicing adipisicing qui consectetur irure fugiat magna. Dolor velit ullamco cillum ipsum ullamco elit do irure id commodo. Irure duis amet do exercitation cupidatat.\r\n",
+    "registered": "2014-01-19T02:30:16 -04:00",
+    "latitude": -18.154193,
+    "longitude": 97.549008,
+    "tags": [
+      "magna",
+      "esse",
+      "consectetur",
+      "pariatur",
+      "eu",
+      "velit",
+      "Lorem"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Juliette Ward"
+      },
+      {
+        "id": 1,
+        "name": "Scott Pratt"
+      },
+      {
+        "id": 2,
+        "name": "Billie Mcfadden"
+      }
+    ],
+    "greeting": "Hello, Helga Hooper! You have 3 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "63e16fb00be588ccc7308f33",
+    "index": 2,
+    "guid": "20251c92-bb60-4fd7-bb30-f36210e0be50",
+    "isActive": true,
+    "balance": "$3,178.18",
+    "picture": "http://placehold.it/32x32",
+    "age": 27,
+    "eyeColor": "green",
+    "name": "Lynette Ball",
+    "gender": "female",
+    "company": "VORTEXACO",
+    "email": "lynetteball@vortexaco.com",
+    "phone": "+1 (833) 501-2557",
+    "address": "309 Brigham Street, Gorst, Northern Mariana Islands, 5350",
+    "about": "Proident minim culpa magna ad sunt pariatur sunt dolore et ut. Commodo eu fugiat minim ea ex magna adipisicing et excepteur et dolor. Magna occaecat elit ipsum duis sit aliquip et fugiat veniam qui. Mollit ex qui aute in exercitation. Labore aute culpa voluptate sit cupidatat anim velit sint voluptate cillum nulla minim aliqua commodo. Quis id anim amet mollit enim reprehenderit velit ad tempor do laboris commodo.\r\n",
+    "registered": "2016-06-04T10:16:04 -03:00",
+    "latitude": 17.776994,
+    "longitude": 173.464815,
+    "tags": [
+      "aliqua",
+      "nostrud",
+      "ullamco",
+      "reprehenderit",
+      "eu",
+      "officia",
+      "culpa"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Lourdes Kelly"
+      },
+      {
+        "id": 1,
+        "name": "Cline Rodgers"
+      },
+      {
+        "id": 2,
+        "name": "Earnestine Stevenson"
+      }
+    ],
+    "greeting": "Hello, Lynette Ball! You have 7 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "63e16fb05a91819069bd69b2",
+    "index": 3,
+    "guid": "4ce944c6-fa5a-4232-a17f-b8bb14d4f69f",
+    "isActive": true,
+    "balance": "$3,433.88",
+    "picture": "http://placehold.it/32x32",
+    "age": 35,
+    "eyeColor": "green",
+    "name": "Justine Larson",
+    "gender": "female",
+    "company": "JETSILK",
+    "email": "justinelarson@jetsilk.com",
+    "phone": "+1 (876) 468-3013",
+    "address": "498 Lancaster Avenue, Crisman, Washington, 6304",
+    "about": "Incididunt magna ex consectetur enim laborum tempor et ullamco irure pariatur id qui. Velit dolore incididunt qui culpa laboris enim voluptate incididunt ex esse duis Lorem pariatur. Sunt exercitation consectetur laborum nulla. Reprehenderit duis cupidatat in ex mollit voluptate exercitation culpa fugiat ut do. Fugiat est irure cillum aute consectetur cillum.\r\n",
+    "registered": "2022-03-26T02:45:16 -03:00",
+    "latitude": -18.57819,
+    "longitude": 31.555956,
+    "tags": [
+      "nisi",
+      "occaecat",
+      "et",
+      "mollit",
+      "reprehenderit",
+      "excepteur",
+      "non"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Benson Ford"
+      },
+      {
+        "id": 1,
+        "name": "Joseph Cochran"
+      },
+      {
+        "id": 2,
+        "name": "Toni Deleon"
+      }
+    ],
+    "greeting": "Hello, Justine Larson! You have 8 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "63e16fb08cbd28338c0b6585",
+    "index": 4,
+    "guid": "747ba6a5-82d2-4a57-a50e-9610f3ab233e",
+    "isActive": false,
+    "balance": "$1,259.56",
+    "picture": "http://placehold.it/32x32",
+    "age": 36,
+    "eyeColor": "blue",
+    "name": "Letha Moses",
+    "gender": "female",
+    "company": "DOGNOSIS",
+    "email": "lethamoses@dognosis.com",
+    "phone": "+1 (842) 431-2107",
+    "address": "526 Alton Place, Kempton, Utah, 563",
+    "about": "Sit consectetur in enim nisi et velit nisi. Est aliqua esse proident non aliquip aliqua commodo cillum proident. Ut adipisicing nostrud Lorem Lorem amet ea tempor. Voluptate reprehenderit officia veniam sunt fugiat anim do. Sunt labore mollit non dolore id eu. Voluptate anim laboris quis qui in ea duis.\r\n",
+    "registered": "2019-08-09T04:11:06 -03:00",
+    "latitude": -81.855702,
+    "longitude": 178.504547,
+    "tags": [
+      "quis",
+      "in",
+      "anim",
+      "pariatur",
+      "pariatur",
+      "cillum",
+      "nulla"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Eugenia Hale"
+      },
+      {
+        "id": 1,
+        "name": "Cervantes Villarreal"
+      },
+      {
+        "id": 2,
+        "name": "Cathleen Lindsey"
+      }
+    ],
+    "greeting": "Hello, Letha Moses! You have 4 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "63e16fb0191d0aa88a287822",
+    "index": 5,
+    "guid": "29dd6670-47d5-40fd-bf35-62f68de94bc3",
+    "isActive": false,
+    "balance": "$2,846.38",
+    "picture": "http://placehold.it/32x32",
+    "age": 37,
+    "eyeColor": "blue",
+    "name": "Tracy Stuart",
+    "gender": "female",
+    "company": "SEQUITUR",
+    "email": "tracystuart@sequitur.com",
+    "phone": "+1 (862) 467-2215",
+    "address": "931 Newel Street, Elwood, Puerto Rico, 2356",
+    "about": "Deserunt irure ex sit do esse incididunt ad laborum veniam elit cillum. Veniam commodo eiusmod quis minim cupidatat ex est enim mollit minim adipisicing. Sunt pariatur eu excepteur velit minim. Reprehenderit fugiat amet excepteur voluptate consectetur aliquip minim consequat pariatur. Dolor magna eiusmod non quis. Consectetur ullamco cillum cillum ex eiusmod sunt excepteur aute et adipisicing aliquip dolor eiusmod incididunt.\r\n",
+    "registered": "2015-03-23T06:00:38 -03:00",
+    "latitude": 21.990213,
+    "longitude": -139.133452,
+    "tags": [
+      "dolore",
+      "ad",
+      "incididunt",
+      "pariatur",
+      "labore",
+      "ea",
+      "ad"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Nieves Humphrey"
+      },
+      {
+        "id": 1,
+        "name": "Bean Walton"
+      },
+      {
+        "id": 2,
+        "name": "Bessie Jackson"
+      }
+    ],
+    "greeting": "Hello, Tracy Stuart! You have 7 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "63e16fb0dad02f340dfa9d35",
+    "index": 6,
+    "guid": "5b1deae5-ae0b-426d-8ea7-9bb8fccea527",
+    "isActive": false,
+    "balance": "$3,823.78",
+    "picture": "http://placehold.it/32x32",
+    "age": 28,
+    "eyeColor": "brown",
+    "name": "Giles Chavez",
+    "gender": "male",
+    "company": "ZILLACOM",
+    "email": "gileschavez@zillacom.com",
+    "phone": "+1 (916) 447-3555",
+    "address": "688 Jamaica Avenue, Chamizal, Marshall Islands, 7350",
+    "about": "Exercitation culpa consequat ipsum laboris minim excepteur Lorem ad. Officia ad ut aliquip ad ullamco voluptate nostrud officia dolore enim. Consectetur cupidatat commodo voluptate ullamco tempor. Sunt et labore quis duis laborum.\r\n",
+    "registered": "2019-08-21T03:27:18 -03:00",
+    "latitude": -28.114651,
+    "longitude": -136.625384,
+    "tags": [
+      "velit",
+      "qui",
+      "ad",
+      "deserunt",
+      "adipisicing",
+      "officia",
+      "non"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Lott Strong"
+      },
+      {
+        "id": 1,
+        "name": "Antonia Kent"
+      },
+      {
+        "id": 2,
+        "name": "Deann Park"
+      }
+    ],
+    "greeting": "Hello, Giles Chavez! You have 2 unread messages.",
+    "favoriteFruit": "banana"
+  }
+]
